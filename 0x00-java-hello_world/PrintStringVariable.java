f2d52da6cf457c5b676c1c5e61f207ad46601801
janetmatshidze <matshidzajanet@gmail.com>
2026-05-18 16:09:39 +0200

    3. Print String Variable

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -2,5 +2,5 @@ public class Main {
   public static void main(String[] args) {
    String text = "Holberton School" ;
     System.out.println(text + text + text);
-    System.out.println(text.substring(0.9));
+    System.out.println(text.substring(0, 9));
   }}
