a16cefb81f12309cb495cdcf8ce6562e7fd8173d
janetmatshidze <matshidzajanet@gmail.com>
2026-05-19 15:16:01 +0200

    4. Concatenation

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-   String text1 = Holberton;
-   String text2 = School;
+   String text1 = "Holberton";
+   String text2 = "School";
 
    System.out.println("Welcome to " + text1 " " + text2 + " !");
  }
