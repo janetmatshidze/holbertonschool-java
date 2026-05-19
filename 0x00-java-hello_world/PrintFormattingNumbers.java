8d1771e6ff1c9768b9a2c7ff95dbd148c3fae23c
janetmatshidze <matshidzajanet@gmail.com>
2026-05-19 15:38:08 +0200

    5. Formatting Numbers

---
+++ 0x00-java-hello_world/PrintFormattingNumbers.java
@@ -0,0 +1,8 @@
+public class PrintFormattingNumbers {
+        public static void main(String[] args) {
+            float tax = 0.2456f;
+            float value = 7654.321f;
+            System.out.println("Value:" $%,.2f%n", value);
+            System.out.println("Tax": %.2f%%%n", tax);
+        }
+    }
