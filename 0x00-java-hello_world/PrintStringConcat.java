bdf2a4c01567a0373aba7614050f4d0c8917f233
janetmatshidze <matshidzajanet@gmail.com>
2026-05-19 15:07:08 +0200

    4. Concatenation

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+   String text1 = Holberton;
+   String text2 = School;
+
+   System.out.println($"Welcome to {text1} {text2} !");
+ }
+}
