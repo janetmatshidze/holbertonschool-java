e51edd7a3172e4047fc299340a5c93765f622249
janetmatshidze <matshidzajanet@gmail.com>
2026-05-21 15:37:44 +0200

    3. Sum

--- 0x01/sum/Number.java
+++ 0x01/sum/Number.java
@@ -1,6 +1,6 @@
 public class Number {
- public static void main (String[] args) {
-    public static int DoSum(int a , int b)
+ public static int DoSum (int a , int b) {
+
      return a + b ;
  }
 }
