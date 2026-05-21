8d2bf247f61639572c766d211c08891cdf75d6ce
janetmatshidze <matshidzajanet@gmail.com>
2026-05-21 15:42:55 +0200

    3. Sum

--- 0x01/sum/Program.java
+++ 0x01/sum/Program.java
@@ -1,5 +1,5 @@
 public class Program {
- public static void main (String[] args) {
+ public static void main(String[] args) {
      System.out.printf("%d\n", TheNumber.DoSum(24, 32));
      System.out.printf("%d\n", TheNumber.DoSum(5, 0));
      System.out.printf("%d\n", TheNumber.DoSum(4, -2));
