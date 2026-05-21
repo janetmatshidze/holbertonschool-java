206366e451f396579d6edde784a485a75ebae90f
janetmatshidze <matshidzajanet@gmail.com>
2026-05-21 15:12:21 +0200

    1. Even numbers from 0 to 99

---
+++ 0x01/even/EvenNumbers.java
@@ -0,0 +1,12 @@
+ public class EvenNumbers {
+  public static void main(String[] args){
+    for (int i = 0; i <=99; i +=2) {
+       if(i < 98) {
+         System.out.print(i + ", ");
+     } else {
+        System.out.print(i);
+   }
+  }
+}
+}
+
