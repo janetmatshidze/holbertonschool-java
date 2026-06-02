a915c743b1c9ce43ff37430e08cf45b9433b79f2
janetmatshidze <matshidzajanet@gmail.com>
2026-06-02 10:42:05 +0200

    1. Encapsulation

---
+++ Classes_Objects/encapsulation/CharacterGame.java
@@ -0,0 +1,25 @@
+public class CharacterGame {
+
+    private int currentHealth;
+    private String name;
+
+    // Getters
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+        public String getName() {
+        return name;
+    }
+
+    //Setters
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+
+}
