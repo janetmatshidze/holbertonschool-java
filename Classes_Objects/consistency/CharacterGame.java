f895abed3d46af0d920d7b94841fac0b0d53cb52
janetmatshidze <matshidzajanet@gmail.com>
2026-06-02 12:38:25 +0200

    4. Consistency

---
+++ Classes_Objects/consistency/CharacterGame.java
@@ -0,0 +1,67 @@
+public class CharacterGame {
+
+    private int currentHealth;
+    private String name;
+    private String status;
+    
+    // Constructor
+    public CharacterGame(int currentHealth, String name){
+       this.name = name;
+       setCurrentHealth(currentHealth);
+    }
+
+    // Getters
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+        public String getName() {
+        return name;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    //Setters
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+
+        if(currentHealth > 0) {
+            status = "alive";
+        }
+        else {
+            status = "dead";
+        }
+    }
+
+    public void setName(String name){
+        if(name == null || name.isEmpty()){
+            return;
+    }
+         this.name = name;
+    }
+
+    public void takeDamage(int damageAmount) {
+      int newHealth = currentHealth - damageAmount;
+
+      if(newHealth < 0) {
+        newHealth = 0; // it becomes 0 because it cannot be negetive .
+     }
+     setCurrentHealth(newHealth);
+}
+
+    public void receiveHealing(int healingAmount) {
+     int newHealth = currentHealth + healingAmount;
+
+       if(currentHealth > 100){
+        currentHealth = 100; // cannot exceed 100%
+       }
+       setCurrentHealth(newHealth);
+    }
+}
+
+   
+
+
+
