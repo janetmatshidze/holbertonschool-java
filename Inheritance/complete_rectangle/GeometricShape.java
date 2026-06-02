cd1de82a91c78812042275114f066bbba5fb86a1
janetmatshidze <matshidzajanet@gmail.com>
2026-06-02 15:07:13 +0200

    3. Complete Rectangle

---
+++ Inheritance/complete_rectangle/GeometricShape.java
@@ -0,0 +1,8 @@
+import java.lang.UnsupportedOperationException;
+
+public class GeometricShape {
+
+    public double area() throws UnsupportedOperationException {
+     throw new UnsupportedOperationException("Function area not implemented");
+    }
+}
---
+++ Inheritance/complete_rectangle/Rectangle.java
@@ -0,0 +1,48 @@
+ public class Rectangle extends GeometricShape {
+
+ private double width;
+ private double height;
+
+ // Getters
+  public double getWidth() {
+    return width;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  // Setters
+  public void setWidth(double width) throws UnsupportedOperationException {
+     this.width = width;
+      if(width <= 0) {
+        throw new UnsupportedOperationException("Width must be greater than or equal to 0");
+      }
+      return;
+
+  }
+
+  public void setHeight(double height) throws UnsupportedOperationException {
+    this.height = height;
+
+    if(height <= 0) {
+        throw new UnsupportedOperationException("Height must be greater than or equal to 0");
+    }
+    return;
+
+  }
+
+  // Overrides the area() method
+
+  @Override
+   public double area() throws UnsupportedOperationException {
+    
+    double area = height * width;
+
+     return area;
+   }
+   
+   public String toString() {
+    return String.format("[Rectangle]  %.2f / %.2f", width, height);
+   }
+}
